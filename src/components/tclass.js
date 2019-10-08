a5b37f4b6a4ba503007e36e4eeea9984372bd41a
chotipat <chotipatp@gmail.com>
2019-10-08 13:25:04 +0700

    20191008_start

---
+++ src/components/tclass.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class tclass extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default tclass;
